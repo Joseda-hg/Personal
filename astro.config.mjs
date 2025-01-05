b025810f2225a04ce247dbdf6184eb0a8afb6efb
Joseda <Josedhg29@gmail.com>
2025-01-05 10:22:37 -0300

    Commented Base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://joseda-hg.github.io',
-    base: 'my-repo',
+    // base: 'my-repo',
 });
